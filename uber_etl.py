9e337998ee54d2f319a6677419eaae55a2b51d40
VARUN KHUMAAR DEIVASIGAMANI <20425351@students.latrobe.edu.au>
2023-07-11 00:31:11 +1000

    Uploaded Files

--- uber_etl.py
+++ uber_etl.py
@@ -4,20 +4,38 @@ def uber_etl():
 
     #Extract: GCP cloud storage
     df=pd.read_csv('https://storage.googleapis.com/uber_data_kronos96/uber_data.csv')
-    
+
     #Transformations#
     df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'], format='%d/%m/%Y %H:%M')
     df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'], format='%d/%m/%Y %H:%M')
 
+    df["Pickup_hour"] = pd.to_datetime(df["tpep_pickup_datetime"]).dt.hour
+    conditions = [(df["Pickup_hour"] >= 0) & (df["Pickup_hour"] < 3),
+        (df["Pickup_hour"] >= 3) & (df["Pickup_hour"] < 6),
+        (df["Pickup_hour"] >= 6) & (df["Pickup_hour"] < 9),
+        (df["Pickup_hour"] >= 9) & (df["Pickup_hour"] < 12),
+        (df["Pickup_hour"] >= 12) & (df["Pickup_hour"] < 15)]
+    choices = ["12am - 3am", "3am - 6am", "6am - 9am", "9am - 12pm", "12pm - 3pm"]
+    df["Pickup_group"] = np.select(conditions, choices, default="other")              #Creating a grouping for pickup hour
+
     df['trip_distance'] = 1.60934*df['trip_distance'] #Miles to Kms
     df['trip_distance'] = df['trip_distance'].round(2)
 
-    df = df[df['total_amount'] > 0] #Removing cancelled trips
+    df = df[(df['pickup_longitude'] != 0) & (df['pickup_latitude'] != 0)]     #Removing incorrect coordinates
+    df = df[(df['dropoff_longitude'] != 0) & (df['dropoff_latitude'] != 0)]   #Removing incorrect coordinates
+
+    df = df[df['total_amount'] > 0]
+
+    mapping = {1: "Standard rate", 2: "JFK", 3: "Newark", 4: "Westchester", 5: "Negotiated fare", 6: "Group ride"}
+    df['RatecodeID'] = df['RatecodeID'].map(mapping)
 
     mapping = {1: "Credit Card", 2 : "Cash", 3 : "Credits", 4 :"Unknown"}
     df['payment_type'] = df['payment_type'].map(mapping)
 
-    columns_to_drop = ['VendorID', 'RatecodeID','store_and_fwd_flag',] 
+    mapping = {1: "Creative Mobile Tech", 2: "VeriFone Inc"}
+    df['VendorID'] = df['VendorID'].map(mapping)
+
+    columns_to_drop = ['store_and_fwd_flag']
     df = df.drop(columns_to_drop, axis=1)
 
     new_columns = {'tpep_pickup_datetime': 'Pickup_Datetime', 'tpep_dropoff_datetime': 'Dropoff_Datetime','total_amount':'Final_total' }
